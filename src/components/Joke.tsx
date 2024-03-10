d322a5e5e41906d52c29993ab14e38e2e088861f
Milan Kovac <113798238+milan-kovac@users.noreply.github.com>
2024-03-10 15:40:09 +0100

    😜 Joke API

--- src/components/Joke.tsx
+++ src/components/Joke.tsx
@@ -1,7 +1,24 @@
+import { useQuery } from "react-query";
+import Spinner from "./Spinner";
+
+const fetchJoke = async () => {
+  const response = await fetch("https://icanhazdadjoke.com/", {
+    headers: { Accept: "application/json" },
+  });
+
+  if (!response.ok) {
+    throw new Error(`Error ${response.status}: Unable to fetch joke from API`);
+  }
+
+  const jokeData = await response.json();
+  return jokeData.joke;
+};
+
 const Joke = () => {
+  const { data: joke, isError, isLoading } = useQuery("joke", fetchJoke);
   return (
-    <div className="bg-white p-2 h-fit justify-center items-center rounded-lg">
-      <h1 className="break-words">＂I used to have a job at a calendar factory but I got the sack because I took a couple of days off.＂</h1>
+    <div className="bg-white p-2 h-fit rounded-lg flex justify-center items-center">
+      {isLoading ? <Spinner /> : <h1 className="break-words">{isError ? "Please try later." : joke}</h1>}
     </div>
   );
 };
